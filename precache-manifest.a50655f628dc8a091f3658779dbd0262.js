c7ddc785c0890a663330e385766c61ea5dcab529
AndreasTetz <16765871+AndreasTetz@users.noreply.github.com>
2020-03-26 13:09:56 +0100

    Updates

---
+++ precache-manifest.a50655f628dc8a091f3658779dbd0262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa709bdbc58ecf4a6338b549645d8401",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa19b478beb2835453de",
+    "url": "/monsters-rolodex/static/css/main.808dcb5e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa19b478beb2835453de",
+    "url": "/monsters-rolodex/static/js/main.206c664d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
